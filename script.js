7db5bbf62ab8727aa31c86b0bc7b12ae0f698f66
jeanine loughlin <jeanine8855@gmail.com>
2021-10-20 09:43:33 -0700

    adding content to script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "On February 22, 2021 I received an email from GitHub to notify me that the use of a 'password' for login for pushing a local repository the remote GitHub ";
+"I was advised that using a 'password' would soon no longer work."
+"I thought it was a vague statement to be coming from GitHub, there was no specific date and it wasn't clear to me."
+"There is a username and password associated with signing in to your GitHub account."
+"And there is also a username and password sometimes requested when pushing from a local repository to the remote GitHub repository."
+"GitHub was referrring the the second instance.  They provided a link to get more information about the change."
+"THE CHANGE:"
+"What the GitHUb change involved was that instead of using your 'password' to push changes from a local repo to the"
+"remote GitHub repo, a user would have to create a new Authentication Access Token"
