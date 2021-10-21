2696f55ca7aa22562cc7598694aca095e32da0f2
jeanine loughlin <jeanine8855@gmail.com>
2021-10-21 07:54:50 -0700

    added to script.js file

--- script.js
+++ script.js
@@ -159,5 +159,16 @@
     When I go over to my GitHub account I see the changes to my files and a time stamp to
     indicate the changes I made using the git command line inputs for the dirctory
     that I created in bash shell for my Visual Studio Code editor are confirmed.
+    In bash shell in the local repository for this project:
+    I entered 'git status' and see:
+
+    On branch main
+    Your branch is up-to-date with 'origin/main'.
+    nothing to commit, working directory clean
+
+    This confirms my local and remote repositories are synced.
+    My personal access token is functional and now replaces use of a password.
+
     And I am now using the Markdown format to style my README.md file.
+
     `
