fd7afa74d37303d0323ccc4981b0388ef2b74e14
jeanine loughlin <jeanine8855@gmail.com>
2021-10-22 07:40:53 -0700

    changes to script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@
 
      * Then I typed 'touch script.js' to create another empty file for the new directory
        (although it probably should have been a text file .txt)
-       I added the content here in Visual Studio Code.
+       I added the content working in Visual Studio Code.
      * Now I have a new local directory (subdirectory actually) containing 2 empty files.
     Next, in bash shell, I typed 'git init' to initialize a new repository that is intended
     to be associated with my GitHub account, and the terminal showed this confirmation:
@@ -51,12 +51,13 @@
 
 
     Next, I go over to GitHub.  I am already logged in and tap on the GtiHub tab icon at the top of
-    my laptop screen.  (A little black octocat favicon)
+    my laptop screen, because I tend to leave tabs open.  (A little black octocat favicon)
+    Now I'm looking at my GitHub homepage.
     At the top of my GitHub page on the right I clicked on the +- icon in between my Github user icon
     and the bell icon.  
     A small drop down menu appears.  I clicked on 'new repository' so I can create my remote
     GitHub repository, that will be associated with my local repository. 
-    A new Gtihub page appears
+    A new Gtihub page appears.
     Near the top - after my GitHub username/ (in the empty space) I typed testing-github-issues 
     to give my new GitHub repository a name related to its' content/purpose.
     (and I am using https:// not ssh)
@@ -83,14 +84,14 @@
 
 
     AND HERE IS WHEN I ENCOUNTERED MY GITHUB ISSUE (It took me a week to reconcile)
-    and before I went into Settings and created my first access token
+    and before I went into Settings and created my first access token.
 
     Instead of getting a confirmation that I had pushed my local repository to my remote Github 
     I saw this:
     remote:  Support for password authehtication was removed on August 13, 2021.  Please use
              a personal access token instead.
     remote:  Please see github.blog/2020-12-15-token-authentication-requirements-for-git-operations/ for more information.
-
+    (I clicked on the link for the 2nd 'remote' to read the update.)
         
     I followed the link and then created my first personal access token.  I saw the token briefly then
     it disappeard from the page and text indicated it would expire in 1 month.
@@ -99,6 +100,7 @@
     I had a brand new personal access token to enter.
     Since GitHub no longer uses a password at this juncture why hasn't the legacy code been
     updated to use 'access token' instead of 'password'?
+    (Although you still have a username/password associated with login a GitHub account.)
 
     But when I entered the access token, it did not work and I saw the same two remote: messages
     above, again.  
@@ -106,7 +108,7 @@
     entered incorrectly intentionally, to see if it would generate an 'incorrect password' amd it 
     did.
     So, I decided, perhaps I made some type of error.  So I created a second (new) access token.
-    At this point, I'm perplexed and frustrated,  so I posted on the Codecademy forum that I was
+    At this point, I'm perplexed and frustrated, so I posted on the Codecademy forum that I was
     having this issue, and saw that others had indicated the same issue.  There was one attempt to
     help but since it's a relative new issue, there wasn't much feedback.
     Next I Googled the issue, but, again, as it is a relatively new as the password to access token was implemented
@@ -124,7 +126,7 @@
     It was NOT the gmail account I created and linked to my GitHub account.
     So I opted to not continue to log in, and instead selected 'cancel'.
     I next received a email confirmation from GitHub to the gmail account that is
-    linked to my GitHub account, confirming I had successfully generated a new
+    linked/associated with my GitHub account, confirming I had successfully generated a new
     access token.
     I coped this access token, by clicking on the blue document icon that is next to the
     last character of this long token (a mix of uppper and lowercase letters and numbers).
@@ -145,8 +147,15 @@
     Nor why the third, but now only existing personal access token, did work.
     (which is still active and now associated with my Github account username/gmail.)
     I followed the same protocol throughout each time.
-    But, I am didn't give up, and even though I repeated the same steps I was eventually
-    successfully, the problem is solved.
+    But, I didn't give up, and even though I repeated the same steps I was eventually
+    successfully, the problem is solved.  Although I'll see as I move forward.
+
+    One additional difference, the first two (unsuccessful) personal access tokens disappeared
+    soon after being created.  In their place a date appeared indicating that each token
+    had an expiration.  But the third token did not disappear.  I can still see it and copy
+    if needed.  And there is not an indicated expiration date associated with this token.
+    That is perplexing, as I generated each token on the same GitHub page the same way.  The
+    protocol is straightforward, so I don't know why there is this deviation.
 
     Perhaps this document/posting will be helpful to someone else with the same issue
     when they are prompted to replace their old password with a new personal access token.
@@ -154,7 +163,7 @@
 
     Finally:
     Now that the recently created personal access token is now associated with GitHub account,
-    I am no longer prompted to enter my 'username' and 'personal access token'
+    I am no longer prompted to enter my 'username' and 'personal access token ( at 'password')'
     when I do a 'git push -u origin main' in bash shell as I added content to this repository.
     When I go over to my GitHub account I see the changes to my files and a time stamp to
     indicate the changes I made using the git command line inputs for the dirctory
@@ -164,13 +173,12 @@
 
     On branch main
     Your branch is up-to-date with 'origin/main'.
-    nothing to commit, working directory clean
+    nothing to commit, working directory clean.
 
     This confirms my local and remote repositories are synced.
     My personal access token is functional and now replaces use of a password.
 
-    And I am now using the Markdown format to style my README.md file.
-    One additional detail:
+    (additionally) I am now using the Markdown language to style my README.md file.
     
     Hopefully, this issue is closed.
 
